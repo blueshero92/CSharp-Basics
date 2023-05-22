4bef098b39e1edecd2828e6d072a406a72ce4c37
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 22:16:56 +0300

    Add files via upload

---
+++ 7. Conditional Statements Advanced More Exercise/05. Vacation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _05._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double price = 0;
+
+            string location = string.Empty;
+            string restType = string.Empty;
+
+            if (budget <= 1000)
+            {
+                restType = "Camp";
+
+                if(season == "Summer")
+                {
+                    price = budget * 0.65;
+                    location = "Alaska";
+                }
+                else
+                {
+                    price = budget * 0.45;
+                    location = "Morocco";
+                }
+            }
+            
+            else if (budget <= 3000)
+            {
+                restType = "Hut";
+
+                if (season == "Summer")
+                {
+                    price = budget * 0.80;
+                    location = "Alaska";
+                }
+                else
+                {
+                    price = budget * 0.60;
+                    location = "Morocco";
+                }
+            }
+            else
+            {
+                restType = "Hotel";
+                price = budget * 0.90;
+
+                if (season == "Summer")
+                {
+                    location = "Alaska";
+                }
+                else
+                {
+                    location = "Morocco";
+                }
+            }
+
+            Console.WriteLine($"{location} - {restType} - {price:f2}");
+        }
+    }
+}
