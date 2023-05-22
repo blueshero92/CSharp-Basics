f0c9d3cc17d529d4b7a4ce2701eb786715e5474f
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 21:24:12 +0300

    Add files via upload

---
+++ Conditional Statemets Exercise/05.GodzillaVsKong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.GodzillaVsKong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int extras = int.Parse(Console.ReadLine());
+            double outfitPrice = double.Parse(Console.ReadLine());
+
+            double scenery = budget * 0.10;
+
+            if (extras > 150)
+            {
+                outfitPrice = outfitPrice - outfitPrice * 0.10;
+            }
+            double totalPrice = extras * outfitPrice + scenery;
+
+            if (totalPrice > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {totalPrice - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - totalPrice:f2} leva left.");
+            }
+               
+        }
+    }
+    
+ }
