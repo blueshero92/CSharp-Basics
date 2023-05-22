5dc78f463d7bc4540f50828c80fdb3c811ef005e
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 21:12:00 +0300

    Add files via upload

---
+++ First Steps in Coding Lab/09.YardGreening.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Reflection.Metadata;
+
+namespace _09.YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double size = double.Parse(Console.ReadLine());
+
+            double price = size * 7.61;
+            double discount = price * 0.18;
+
+            Console.WriteLine($"The final price is:  {price-discount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv");
+
+        }
+    }
+}
