dab642591a7d230579c87db0e2774cc5f69e0fe0
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 22:47:17 +0300

    Add files via upload

---
+++ 10. For Loop More Exercise/03. SumNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int numbersSum = 0;
+            int input;
+
+            while(numbersSum < number)
+            {
+                input = int.Parse(Console.ReadLine());
+                
+                numbersSum = numbersSum + input;
+            }
+
+            Console.WriteLine(numbersSum);
+        }
+    }
+}
---
+++ 10. For Loop More Exercise/08.Graduation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _08.Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int grade = 1;
+            double mark = 0;
+
+            double markSum = 0;
+            int gradeRepeated = 0;
+            double averageMark = 0;
+            
+            while(grade <= 12)
+            {
+                mark = double.Parse(Console.ReadLine());
+
+                if (mark >= 4)
+                {
+                    grade++;
+                }
+                else
+                {
+                    gradeRepeated++;
+                }
+                if(gradeRepeated > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {grade} grade");
+                    break;
+                }
+
+                averageMark = averageMark + mark / 12;
+            }
+            if(grade > 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {averageMark:f2}");
+            }
+        }
+    }
+}
