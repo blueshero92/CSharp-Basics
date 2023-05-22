05094b604b2f33a5b4e119169d20c53d3199a78e
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 22:13:39 +0300

    Add files via upload

---
+++ 6. Conditional Statements Advanced Exercise/07. Hotel Room.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nightCount = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            if (month == "May" || month == "October")
+            {
+                studioPrice = 50;
+                apartmentPrice = 65;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studioPrice = 75.20;
+                apartmentPrice = 68.70;
+            }
+            else
+            {
+                studioPrice = 76;
+                apartmentPrice = 77;
+            }
+
+            double studioPriceTotal = studioPrice * nightCount;
+            double apartmentPriceTotal = apartmentPrice * nightCount;
+
+            if ((nightCount > 7 && nightCount <= 14) && (month == "May" || month == "October"))
+            {
+                studioPriceTotal -= studioPriceTotal * 0.05;
+            }
+            else if (nightCount > 14 && (month == "May" || month == "October"))
+            {
+                studioPriceTotal -= studioPriceTotal * 0.30;
+            }
+            else if (nightCount > 14 && (month == "June" || month == "September"))
+            {
+                studioPriceTotal -= studioPriceTotal * 0.20;
+            }
+            if (nightCount > 14)
+            {
+                apartmentPriceTotal -= apartmentPriceTotal * 0.10;
+            }
+
+            Console.WriteLine($"Apartment: {apartmentPriceTotal:f2} lv.");
+            Console.WriteLine($"Studio: {studioPriceTotal:f2} lv.");
+
+        }
+    }
+}
---
+++ 6. Conditional Statements Advanced Exercise/08. On Time For Exam.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _08._On_Time_For_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMin = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMin = int.Parse(Console.ReadLine());
+
+            examMin = examMin + examHour * 60;
+            arrivalMin = arrivalMin + arrivalHour * 60;
+
+            int difference = examMin - arrivalMin;
+
+            if (difference >= 0 && difference <= 30)
+            {
+                Console.WriteLine("On time");
+
+                if (difference > 0)
+                {
+                    Console.WriteLine($"{difference} minutes before the start");
+                }
+
+            }
+            else if (difference < 0)
+            {
+                Console.WriteLine("Late");
+
+                if (difference > -60)
+                {
+                    Console.WriteLine($"{Math.Abs(difference)} minutes after the start");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{Math.Abs(difference / 60)}:{Math.Abs(difference % 60):d2} hours after the start");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Early");
+
+                if (difference >= 60)
+                {
+                    Console.WriteLine($"{Math.Abs(difference / 60)}:{difference % 60:d2} hours before the start");
+                }
+                else
+                {
+                    Console.WriteLine($"{difference} minutes before the start");
+                }
+            }
+                  
+                
+            
+        }
+    }
+}
