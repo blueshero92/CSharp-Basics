6030c1c8571d0d19a443bfcbf3ffabe706917d10
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-22 21:40:11 +0300

    Add files via upload

---
+++ 4. Conditional Statemets Exercise/07.Shopping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int gpus = int.Parse(Console.ReadLine());
+            int cpus = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double gpuPrice = 250;
+            double cpuPrice = gpuPrice*gpus * 0.35;
+            double ramPrice = gpuPrice*gpus * 0.10;
+
+            double totalSum = gpus*gpuPrice + cpus*cpuPrice + ram*ramPrice;
+
+            if (gpus > cpus)
+            {
+                totalSum = totalSum - totalSum * 0.15;
+            }
+            if (budget >= totalSum)
+            {
+                Console.WriteLine($"You have {budget - totalSum:f2} leva left!");
+
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {totalSum - budget:f2} leva more!");
+            }
+        }
+    }
+}
---
+++ 4. Conditional Statemets Exercise/08.LunchBreak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string series = Console.ReadLine();
+            int episodeTime = int.Parse(Console.ReadLine());
+            int lunchBreak = int.Parse(Console.ReadLine());
+
+            double lunch = lunchBreak * 0.125;
+            double rest = lunchBreak * 0.25;
+            double timeWatching = lunchBreak - (lunch + rest);
+
+            if (episodeTime <= timeWatching)
+            {
+                Console.WriteLine($"You have enough time to watch {series} and left with {Math.Ceiling(timeWatching - episodeTime)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {series}, you need {Math.Ceiling(episodeTime - timeWatching)} more minutes.");
+            }
+        }
+    }
+}
